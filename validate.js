bee4049abd7e4ae32d872ce8f78aae2f73087263
mhaluphet129 <71627281+mhaluphet129@users.noreply.github.com>
2020-10-08 15:32:29 +0800

    Update validate.js

--- validate.js
+++ validate.js
@@ -6,11 +6,7 @@ $(document).ready(()=>{
          let email = $("#email").val();
          let username = $("username").val();
          
-         if ( pass != confirm )
-              alert("Passwords are not match.)
-         else if ( pass == '' || confirm == '' || name == '' || email == '' || username == '' )
-              alert("Provide the blank spaces.");
-         else
+     
               alert("Name: " + name + "\nEmail:" + email + "\nUsername:" + username + "\nPassword: " + password );
 
          /*
