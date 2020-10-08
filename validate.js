1ff1d96a3c1b8b5bb67a973e147ed45cce08f79e
mhaluphet129 <71627281+mhaluphet129@users.noreply.github.com>
2020-10-08 15:42:18 +0800

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,15 +1,7 @@
 $(document).ready(()=>{
      function validate(){
  alert("start");
-         let pass = $("#password").val();
-         let confirm = $("#confirm").val();
-         let name = $("#name").val();
-         let email = $("#email").val();
-         let username = $("#username").val();
          
-     
-              alert("Name: " + name + "\nEmail:" + email + "\nUsername:" + username + "\nPassword: " + password );
-
          /*
           Alli is cute
          */
