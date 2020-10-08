a69e4aeb5b5cfb66f5fe2137169020b2d508aa67
mhaluphet129 <71627281+mhaluphet129@users.noreply.github.com>
2020-10-08 15:21:52 +0800

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,17 +1,6 @@
 $(document).ready(()=>{
      function validate(){
  alert("cute si bujoy");
-         let pass = $("#password").val();
-         let confirm = $("#confirm").val();
-         let name = $("#name").val();
-         let email = $("#email").val();
-         let username = $("username").val();
-         
-         if ( pass != confirm )
-              alert("Passwords are not match.)
-         else
-              alert("Name: " + name + "\nEmail:" + email + "\nUsername:" + username + "\nPassword: " + password );
-
          /*
           Alli is cute
          */
