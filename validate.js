617a56b54d6361d5e74c02b03372636d765222fd
mhaluphet129 <71627281+mhaluphet129@users.noreply.github.com>
2020-10-10 00:40:39 +0800

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,12 +1,18 @@
-$(document).ready(()=>{
-     function validate(){
- alert("start");
-         
-         /*
-          Alli is cute
-         */
-     }
+let name, username, email, password, confirm;
 
+let init = () =>{
+    name = $("#name").val();
+    email = $("#email").val();
+    username = $("#username").val();
+    password = $("#password").val();
+    confirm = $("#confirm").val();
+}
 
-     $("#register").click(validate);
-})
+let validate = () => {
+    alert(name+" "+email+" "+username+" "+password+" "+confirm);
+}
+
+$(".login-button").click(validate);
+
+$(document).ready(init);
+
