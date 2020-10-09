083b911b60d9a1fd084a5310ef0d9bc47c2b0b9a
mhaluphet129 <71627281+mhaluphet129@users.noreply.github.com>
2020-10-10 01:02:56 +0800

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,12 +1,1 @@
-let name, username, email, password, confirm;
-
-    name = $("#name").val();
-    email = $("#email").val();
-    username = $("#username").val();
-    password = $("#password").val();
-    confirm = $("#confirm").val();
-
-
-    alert(name+" "+email+" "+username+" "+password+" "+confirm);
-
 
